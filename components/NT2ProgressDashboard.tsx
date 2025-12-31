12858f2a8d1c5acc86c9449bcefbdb23797705b5
Imad <em20182019@gmail.com>
2025-12-31 21:27:49 +0100

    Fix: Add useRef import for request deduplication

--- components/NT2ProgressDashboard.tsx
+++ components/NT2ProgressDashboard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useTranslations, useLocale } from 'next-intl'
 import { type Block36Metrics } from '@/lib/nt2-scoring'
 import { CheckCircle2, XCircle, ChevronDown, ChevronUp } from 'lucide-react'
